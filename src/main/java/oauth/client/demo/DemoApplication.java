2620f48c9b1b26b74a329afccb63f8f86ede0c7b
Mariusz Bogucki <mariubog@gmail.com>
2015-01-11 15:42:58 -0500

    <commit message>

--- src/main/java/oauth/client/demo/DemoApplication.java
+++ src/main/java/oauth/client/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
@@ -24,6 +25,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableAutoConfiguration
 @EnableOAuth2Client
 @EnableAsync
+@PropertySource("classpath:resource.properties")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
