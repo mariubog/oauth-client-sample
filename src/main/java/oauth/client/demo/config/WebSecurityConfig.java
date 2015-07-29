77ba4fcc2deffce8825b3c38eb6f22b9269be57c
mariubog <mariubog@gmail.com>
2015-07-29 19:35:19 -0400

    Update WebSecurityConfig.java

--- src/main/java/oauth/client/demo/config/WebSecurityConfig.java
+++ src/main/java/oauth/client/demo/config/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/results*").permitAll()
 				.anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+				.permitAll().and().logout().permitAll();
 	}
 
 	@Autowired
