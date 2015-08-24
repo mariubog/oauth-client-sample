4cadc2b03fbfa1c9171cbcd5c1c08e49936d3165
mariubog <mariubog@gmail.com>
2015-08-24 15:20:58 -0400

    fixed credentials

--- src/main/java/oauth/client/demo/config/WebSecurityConfig.java
+++ src/main/java/oauth/client/demo/config/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
 			throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password")
+		auth.inMemoryAuthentication().withUser("roy").password("spring")
 				.roles("USER");
 	}
 }
